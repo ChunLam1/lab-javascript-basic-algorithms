ecd1ed617aaca4f9c72252947f74dc6501287e35
Chun Lam <Chun01@hotmail.fr>
2021-12-01 15:46:22 +0100

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
 // 
-
+var hacker1="Chun"
+console.log(`the drive's name is ${hacker1}`);
+var hacker2="Henri-Pierre"
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
+function longestName(driver,conductor){
+    if()
+}
 
 // Iteration 3: Loops
